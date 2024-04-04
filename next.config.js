3b3e5d927f1c3db2f475bfcd026dd95e8fb7f6e6
Maxime <mduhamel31@gmail.com>
2024-04-04 13:17:24 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,8 @@
+// @ts-check
+ 
 /** @type {import('next').NextConfig} */
-
-const withPlugins = require('next-compose-plugins')
-const nextTranslate = require('next-translate-plugin')
-
-module.exports = withPlugins(
-  [
-    nextTranslate
-  ],
-  {
-    //basePath: "/cv",
-    //output: "export",  // <=== enables static exports
-    //images: {
-      //unoptimized: true,
-    //},
-    reactStrictMode: true,
-  }
-)
+const nextConfig = {
+  /* config options here */
+}
+ 
+module.exports = nextConfig
